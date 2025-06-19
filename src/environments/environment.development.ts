007b86fe35ca51436359b248881096e577556872
Alfonso Ramírez <a24rameal@iesgrancapitan.org>
2025-06-19 17:31:19 +0200

    Creacion enviroments y alias

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  companyName: 'Power',
+  companyName2: 'Core',
+  companySlogan: 'Controla el Powerlifting',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  companyName: 'Power',
+  companyName2: 'Core',
+  companySlogan: 'Maneja el Power',
+};

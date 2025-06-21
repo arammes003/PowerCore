81b4f64095a965310609359f7edef898e1d623fc
Alfonso Ramírez <a24rameal@iesgrancapitan.org>
2025-06-21 22:56:32 +0200

    Pagina login con validacion de datos

--- src/app/auth/pages/login-register/login-page.component.ts
+++ src/app/auth/pages/login-register/login-page.component.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login-page.component.html',
 })
-export class LoginPageComponent {}
+export class LoginPageComponent {
+  formBuilder = inject(FormBuilder);
+  hasError = signal(false);
+  isPosting = signal(false);
+
+  loginForm = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      this.hasError.set(true);
+      setTimeout(() => {
+        this.hasError.set(false);
+      }, 2000);
+      return;
+    }
+
+    const { email, password } = this.loginForm.value;
+
+    console.log(email, password);
+  }
+}
